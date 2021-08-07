494a27a39aa6ed2605f72b650ff73da8521c4a5d
Ruchita Kalantri <rkalantri.kalantri@yahoo.com>
2021-08-07 11:37:37 +0530

    using error handeling middleware coreectly inside app.js

--- app.js
+++ app.js
@@ -43,6 +43,13 @@ app.use(
 app.use(csrfProtection);
 app.use(flash());
 
+app.use((req,res,next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+
+
 app.use((req, res, next) => {
   if (!req.session.user) {
     return next();
@@ -56,15 +63,8 @@ app.use((req, res, next) => {
       next();
     })
     .catch(err => {
-      //technical issue
-      throw new Error(err);
+      next(new Error(err));
     });
-});
-
-app.use((req,res,next) => {
-  res.locals.isAuthenticated = req.session.isLoggedIn;
-  res.locals.csrfToken = req.csrfToken();
-  next();
 });
 
 app.use('/admin', adminRoutes);
@@ -77,7 +77,12 @@ app.use(errorController.get404);
 
 // error handeling middleware
 app.use((error , req , res , next) => {
-  res.redirect('/500');
+  //res.redirect('/500');
+  res.status(500).render('500', {
+    pageTitle: 'Error!!!',
+    path: '/500',
+    isAuthenticated: req.session.isLoggedIn
+  });
 });
 
 mongoose
